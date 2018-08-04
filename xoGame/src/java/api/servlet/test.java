006b0d5c2a08382cd74408af44f3de0d233bd45b
l3owNeko <bow.snowich@gmail.com>
2018-08-04 11:44:35 +0700

    test

---
+++ xoGame/src/java/api/servlet/test.java
@@ -0,0 +1,15 @@
+package api.servlet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bow
+ */
+public class test {
+    
+}
